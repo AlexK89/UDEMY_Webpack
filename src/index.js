a4dd6d9412ae39f69d8ea280597089858706a2a1
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-26 21:59:44 +0300

    on demand code loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import sum from './sum'; //ES2015 syntax
-import './image_viewer'; //ES2015 syntax
+// import sum from './sum'; //ES2015 syntax
+// import './image_viewer'; //ES2015 syntax
+//
+// const total = sum(2, 5);
+//
+// console.log(total);
 
-const total = sum(2, 5);
 
-console.log(total);
+const button = document.createElement('button');
+button.innerText = 'Click';
+document.body.appendChild(button);
+button.addEventListener('click', () => {
+	System.import('./image_viewer'); // importing external JS inside the code, not a beginning of file
+});
