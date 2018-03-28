d6ffb0fc5b15c27ea7ec389e96cd284436daf6ca
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-28 09:21:56 +0300

    js file size optimization

--- Webpack_2.0_real_example/webpack.config.js
+++ Webpack_2.0_real_example/webpack.config.js
@@ -1,11 +1,18 @@
 var webpack = require('webpack');
 var path = require('path');
+const VENDOR_LIBS = [ //creating an array of dependencies to optimize loading time by dividing files
+	"react", "lodash", "redux", "react-redux", "react-dom", "faker",
+	"react-input-range", "react-router",  "redux-form", "redux-thunk"
+];
 
 module.exports = {
-	entry: './src/index.js',
+	entry: {
+		bundle: './src/index.js',
+		vendor: VENDOR_LIBS
+	},
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: '[name].js' // [name] - takes entry property as a name of file
 	},
 	module: {
 		rules: [
@@ -13,6 +20,10 @@ module.exports = {
 				use: 'babel-loader',
 				test: /\.js$/,
 				exclude: /node_modules/
+			},
+			{
+				use: ['style-loader', 'css-loader'],
+				test: /\.css$/
 			}
 		]
 	}
