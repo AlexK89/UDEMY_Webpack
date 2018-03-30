f5375d04a53bf396f805f82f3f6fd809a4102961
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-30 14:18:49 +0300

    Node+Webpack integration: use webpack+express just for development

--- Webpack_2.0_real_example/server.js
+++ Webpack_2.0_real_example/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-
-// We need 3 dependencies webpackMiddleware, webpack and webpackConfig to make work webpack with express
-
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
-
+const path = require('path');
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+if (process.env.NODE_ENV !== "production") {
+	// We need 3 dependencies webpackMiddleware, webpack and webpackConfig to make work webpack with express
+
+	const webpackMiddleware = require('webpack-dev-middleware');
+	const webpack = require('webpack');
+	const webpackConfig = require('./webpack.config.js');
+
+	app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+	// Send correct files to user
+	app.use(express.static('dist'));
+
+	// double check if the browser history library works correctly 
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'dist/index.html'));
+	})
+}
+
 app.listen(3050, () => console.log('Listening')); 	// 3050 - is a port which our App should listen for HTTP requests
