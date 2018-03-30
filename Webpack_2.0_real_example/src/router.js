b0bf6db0cf839c1e79faf575806f1b46eb5bba8d
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-30 11:12:32 +0300

    optimisation for React: routing

--- Webpack_2.0_real_example/src/router.js
+++ Webpack_2.0_real_example/src/router.js
@@ -3,9 +3,6 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
 
 // React optimization - code splitting
 
@@ -16,23 +13,31 @@ const componentsRoutes = {
     childRoutes: [
         {
             path: 'artists/new',
-            getComponent(location, cb) {        //getting child components async
-
+            getComponent(location, cb) {        //getting child components async. React calling this function automatically
+                System.import('./components/artists/ArtistCreate')
+                    .then(module => cb(null, module.default))       // cb - callback function
             }
-        }
+        },
+		{
+			path: 'artists/:id',
+			getComponent(location, cb) {        //getting child components async. React calling this function automatically
+				System.import('./components/artists/ArtistDetail')
+					.then(module => cb(null, module.default))       // cb - callback function
+			}
+		},
+		{
+			path: 'artists/:id/edit',
+			getComponent(location, cb) {        //getting child components async. React calling this function automatically
+				System.import('./components/artists/ArtistEdit')
+					.then(module => cb(null, module.default))       // cb - callback function
+			}
+		}
     ]
 };
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={componentsRoutes}/>
   );
 };
 
