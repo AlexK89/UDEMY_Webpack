a31a8d31a3956f0ea9d0cda6d9d422d72aba0571
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-30 13:15:21 +0300

    Node+Webpack integration: express module

--- Webpack_2.0_real_example/server.js
+++ Webpack_2.0_real_example/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 
+// We need 3 dependencies webpackMiddleware, webpack and webpackConfig to make work webpack with express
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('webpack.config');
+
 const app = express();
 
+app.use(webpackMiddleware(webpack(webpackConfig)));
 app.listen(3050, () => console.log('Listening')); 	// 3050 - is a port which our App should listen for HTTP requests
