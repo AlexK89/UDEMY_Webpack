6928e469cb18bbb068a597109195121f533a70fb
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-03-31 16:24:10 +0300

    Heroku + Webpack

--- Webpack_2.0_real_example/server.js
+++ Webpack_2.0_real_example/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Server Routes
+// should be defined !!! BEFORE !!! "if" statement
+app.get('/hello', (req, res) => res.send({hi: 'ther'}));
+
 if (process.env.NODE_ENV !== "production") {
 	// We need 3 dependencies webpackMiddleware, webpack and webpackConfig to make work webpack with express
 
@@ -14,10 +18,11 @@ if (process.env.NODE_ENV !== "production") {
 	// Send correct files to user
 	app.use(express.static('dist'));
 
-	// double check if the browser history library works correctly 
+	// double check if the browser history library works correctly
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'dist/index.html'));
 	})
 }
 
-app.listen(3050, () => console.log('Listening')); 	// 3050 - is a port which our App should listen for HTTP requests
+// We use process.env.PORT as some server provider do not allow you to change it
+app.listen(process.env.PORT || 3050, () => console.log('Listening')); 	// 3050 - is a port which our App should listen for HTTP requests
